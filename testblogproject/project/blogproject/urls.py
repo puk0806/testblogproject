24bbbd41f8ead7f71d89a13d9c592dc860b345ac
chanho <puk0806@naver.com>
2019-05-03 19:53:02 +0900

    3th commit add loginservice

--- testblogproject/project/blogproject/urls.py
+++ testblogproject/project/blogproject/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 import blog.views
 import portfolio.views
+import accounts.views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,7 +10,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name = 'home' ),
     path('blog/', include('blog.urls')),
+    path('accounts/', include('accounts.urls')),
     path('portpolio/',portfolio.views.portfolio, name="portfolio"),
+    
 ] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # 위에 처럼 하던가 아래처럼 
