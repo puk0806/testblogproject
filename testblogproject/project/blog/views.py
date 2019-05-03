a8b793b93608c9bfbd4e43d8b7cd13347169861d
chanho <puk0806@naver.com>
2019-05-03 20:15:44 +0900

    4th commit add pageservice

--- testblogproject/project/blog/views.py
+++ testblogproject/project/blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
 
 def home(request):
     blogs = Blog.objects    # 쿼리셋 # 메소드
-    return render(request, 'home.html', {'blogs':blogs})
+
+    #블로그 페이지 3개씩 찍기
+    blog_list = Blog.objects.all()
+    paginator = Paginator(blog_list, 3)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
+    return render(request, 'home.html', {'blogs':blogs,'posts':posts})
 
 def detail(request, blog_id) :
     details = get_object_or_404(Blog, pk = blog_id)
